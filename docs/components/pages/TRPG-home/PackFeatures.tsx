7df97ab0e6f30b84216b802d3cc6f4c61bfdebae
简律纯 <i@jyunko.cn>
2023-07-09 19:36:30 +0800

    🦴

--- docs/components/pages/TRPG-home/PackFeatures.tsx
+++ docs/components/pages/TRPG-home/PackFeatures.tsx
@@ -4,7 +4,8 @@ import { FeaturesBento } from "../home-shared/FeaturesBento";
 export function PackFeatures() {
   return (
     <FeaturesBento
-      header="Why HydroRollCore?\n为什么选择水系核心?"
+      header="Why HydroRollCore?
+      为什么选择水系核心?"
       body="水系核心基于规则包运行，可自定义程度高，且运行速率快。"
       features={PACK_HOME_FEATURES}
     />
