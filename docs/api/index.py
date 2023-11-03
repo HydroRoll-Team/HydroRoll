9f0d43fe099a95ab1516ae951dcb60a89e76a5a5
简律纯 <i@jyunko.cn>
2023-11-03 21:13:33 +0800

    chore: delete useless codes

--- docs/api/index.py
+++
@@ -1,9 +0,0 @@
-from sanic import Sanic
-from sanic.response import json
-app = Sanic()
- 
- 
-@app.route('/')
-@app.route('/<path:path>')
-async def index(request, path=""):
-    return json({'hello': path})
