7894ed73f929c6676242da3a1c3bd11d78d8e711
简律纯 <i@jyunko.cn>
2023-11-20 09:15:53 +0800

    feat: 实现在 lua 脚本中调用被注册的 Python 同步函数与类

--- tests/lua_in_python.py
+++ tests/lua_in_python.py
@@ -9,4 +9,27 @@ except ImportError:
         except ImportError:
             import lupa
 
-print(f"Using {lupa.LuaRuntime().lua_implementation} (compiled with {lupa.LUA_VERSION})")
+print(
+    f"Using {lupa.LuaRuntime().lua_implementation} (compiled with {lupa.LUA_VERSION})"
+)
+
+from lupa import LuaRuntime
+
+lua = LuaRuntime(unpack_returned_tuples=True)
+import asyncio
+
+async def get_Dice_Dir():
+    import os
+    await asyncio.sleep(1)
+    return os.path.dirname(os.path.abspath(__file__))
+
+
+lua.globals().getDiceDir = get_Dice_Dir
+
+print(
+    lua.eval(
+        """\
+    getDiceDir()
+               """
+    )
+)
