4c08ec908bfaba35c6055ee273a0768ba6749089
简律纯 <i@jyunko.cn>
2023-11-20 09:01:31 +0800

    feat(tests): 实现对 `Lua5.4` 的封装

--- tests/lua_in_python.py
+++ tests/lua_in_python.py
@@ -1,3 +1,12 @@
-import lupa
+try:
+    import lupa.luajit2 as lupa
+except ImportError:
+    try:
+        import lupa.lua54 as lupa
+    except ImportError:
+        try:
+            import lupa.lua53 as lupa
+        except ImportError:
+            import lupa
 
 print(f"Using {lupa.LuaRuntime().lua_implementation} (compiled with {lupa.LUA_VERSION})")
