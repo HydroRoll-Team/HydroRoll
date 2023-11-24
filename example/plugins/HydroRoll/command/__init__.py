2a09acfb60d77e5f4fb3db00cf3f92369086e87c
简律纯 <i@jyunko.cn>
2023-11-25 01:03:34 +0800

    refactor(Lexer): 词法分析器添加`advance`方法

--- example/plugins/HydroRoll/command/__init__.py
+++ example/plugins/HydroRoll/command/__init__.py
@@ -1,5 +1,6 @@
 SET, GET, ALIAS = ("SET", "GET", "ALIAS")
-
+INTEGER = "INTEGER"
+EOF = "EOF"
 
 class Token(object):
     def __init__(self, type, value):
@@ -21,3 +22,53 @@ class Lexer(object):
 
     def error(self):
         raise Exception("Invalid Character")
+
+    def advance(self):
+        """Advance the `pos` pointer and set the `current_char` variable."""
+        self.pos += 1
+        if self.pos > len(self.text) - 1:
+            self.current_char = None  # Indicates end of input
+        else:
+            self.current_char = self.text[self.pos]
+
+    def skip_whitespace(self):
+        while self.current_char is not None and self.current_char.isspace():
+            self.advance()
+
+    def integer(self):
+        """Return a (multidigit) integer consumed from the input."""
+        result = ""
+        while self.current_char is not None and self.current_char.isdigit():
+            result += self.current_char
+            self.advance()
+        return int(result)
+
+    def get_next_token(self):
+        """Lexical analyzer (also known as scanner or tokenizer)
+
+        This method is responsible for breaking a sentence
+        apart into tokens. One token at a time.
+        """
+        while self.current_char is not None:
+            if self.current_char.isspace():
+                self.skip_whitespace()
+                continue
+
+            if self.current_char.isdigit():
+                return Token(INTEGER, self.integer())
+
+            if self.current_char == "get":
+                self.advance()
+                return Token(GET, "get")
+
+            if self.current_char == "set":
+                self.advance()
+                return Token(SET, "set")
+            
+            if self.current_char == "Alias":
+                self.advance()
+                return Token(ALIAS, "Alias")
+            
+            self.error()
+
+        return Token(EOF, None)
