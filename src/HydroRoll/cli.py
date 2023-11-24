49de6873b403d5dfc84f7d1e98bd8431e50e6a6b
简律纯 <i@jyunko.cn>
2023-11-24 09:37:41 +0800

    feat(cli): 实现`install_package` 与 `build_template`

--- src/HydroRoll/cli.py
+++ src/HydroRoll/cli.py
@@ -1,4 +1,6 @@
 import argparse
+import os
+import requests
 
 class Cli(object):
     parser = argparse.ArgumentParser(description="水系终端脚手架")
@@ -20,8 +22,65 @@ class Cli(object):
 
     def get_help(self):
         return self.parser.format_help()
+    
+    def install_packages(self):
+        package_name = input("请输入要安装的包名：")
+        url = f"https://pypi.org/pypi/{package_name}/json"
+        response = requests.get(url)
 
+        if response.status_code == 200:
+            self._extracted_from_install_packages_7(response, package_name)
+        else:
+            print(f"找不到包：{package_name}")
 
-cli = Cli()
+    # TODO Rename this here and in `install_packages`
+    def _extracted_from_install_packages_7(self, response, package_name):
+        data = response.json()
+        latest_version = data["info"]["version"]
+        download_url = data["releases"][latest_version][0]["url"]
 
-print(cli.get_help())
+        plugins_dir = "plugins"
+        if not os.path.exists(plugins_dir):
+            os.mkdir(plugins_dir)
+
+        file_name = download_url.split("/")[-1]
+        file_path = os.path.join(plugins_dir, file_name)
+
+        with open(file_path, "wb") as file:
+            file.write(requests.get(download_url).content)
+
+        print(f"成功安装包：{package_name}")
+    
+    def build_template(self):
+        template = input("请选择应用模板（输入数字）:\n"
+                         "1. 创建轻量应用\n"
+                         "2. 创建标准应用\n"
+                         "3. 创建开发应用\n")
+        
+        if template == "1":
+            print("选择了轻量应用模板")
+        elif template == "2":
+            print("选择了标准应用模板")
+        elif template == "3":
+            print("选择了开发应用模板")
+        else:
+            print("无效的模板选择")
+    
+    def search_package(self):
+        search_term = input("请输入要搜索的包名关键字：")
+        url = f"https://pypi.org/search/?q={search_term}"
+        response = requests.get(url)
+        
+        if response.status_code == 200:
+            data = response.json()
+            packages = data["results"]
+            
+            for package in packages:
+                name = package["name"]
+                topics = package.get("topics", [])
+                
+                if search_term.lower() in name.lower() and "HydroRoll" in topics:
+                    print(f"包名：{name}")
+        else:
+            print(f"搜索失败")
+
