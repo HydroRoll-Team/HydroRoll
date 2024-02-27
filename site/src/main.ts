6b7f2e80670257895dfe7ab01ba948ce21f840f5
白咕咕 <2432115441@qq.com>
2024-02-27 23:06:57 +0800

    导入element-plus

--- site/src/main.ts
+++ site/src/main.ts
@@ -8,7 +8,7 @@
 // Components
 import App from './App.vue'
 
-// Composables
+// Composable
 import { createApp } from 'vue'
 
 // Plugins
@@ -19,8 +19,9 @@ import VMdPreview from '@kangc/v-md-editor/lib/preview';
 import '@kangc/v-md-editor/lib/style/preview.css';
 import githubTheme from '@kangc/v-md-editor/lib/theme/github.js';
 import '@kangc/v-md-editor/lib/theme/style/github.css';
-
-// highlightjs
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+// highlights
 import hljs from 'highlight.js';
 
 VMdPreview.use(githubTheme, {
@@ -31,6 +32,7 @@ VMdPreview.use(githubTheme, {
 
 
 const app = createApp(App)
-app.use(VMdPreview);
+app.use(VMdPreview)
+app.use(ElementPlus)
 registerPlugins(app)
 app.mount('#app')
