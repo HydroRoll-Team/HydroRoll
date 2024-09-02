1605bd66ab97a206e4698954d47b10d87b09ec2d
Ntkskwk <natsukawa247@outlook.com>
2024-09-03 00:03:19 +0800

    feat: add router-guards for site

--- site/src/router/index.ts
+++ site/src/router/index.ts
@@ -4,18 +4,13 @@ import { createRouter, createWebHistory } from 'vue-router'
 const routes = [
   {
     path: '/',
-    component: () => import('@/layouts/default/Default.vue'),
-    children: [
-      {
-        path: '',
-        name: 'Home',
-        // route level code-splitting
-        // this generates a separate chunk (about.[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
-        component: () => import(/* webpackChunkName: "home" */ '@/views/Home.vue'),
-      },
-    ],
+    component: () => import('@/views/Welcome.vue'),
   },
+  {
+    path: '/:path(.*)',
+    name: '404',
+    component: () => import('@/views/404.vue'),
+  }
 ]
 
 const router = createRouter({
