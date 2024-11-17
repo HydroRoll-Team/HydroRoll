b1294e435e5660f177a88b746f360a6b803a8fc2
HsiangNianian <admin@asynth.in>
2024-11-17 09:49:08 +0800

    refactor(conf.py): Split import statements for clarity

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -3,7 +3,8 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-import os, sys
+import os
+import sys
 
 if sys.version_info >= (3, 11):
     import tomllib
